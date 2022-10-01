a5472103a13c60f9ff14d0a932066895412e9ab0
AlbertKloster <albert.kloster@gmail.com>
2022-09-30 23:04:26 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Portfolio
+
+## JetBrains Academy Frontend Core Project
+
+### 1 DOM manipulation
+### 2 Modals with CSS only
+### 3 Responsiveness
---
+++ assets/js/main.js
@@ -0,0 +1,5 @@
+const menuHandler = () => {
+  document.getElementById("dropdown").classList.toggle("hidden");
+  document.getElementById("cross").classList.toggle("hidden");
+  document.getElementById("hamburger").classList.toggle("hidden");
+}
